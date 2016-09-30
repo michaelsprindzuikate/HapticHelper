d20ebb8e5d0d95d71dd99e04ff4b31bb23fa972d
michaelsprindzuikate <msprindzuikate@gmail.com>
2016-09-30 13:51:08 +0100

    Added the files

---
+++ UIControl+HapticHelper.h
@@ -0,0 +1,14 @@
+//
+//  UIControl+HapticHelper.h
+//
+//  Created by Michael Sprindzuikate on 30/09/2016.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIControl (HapticHelper)
+
+
+- (void)enableHapticFeedback;
+
+@end
